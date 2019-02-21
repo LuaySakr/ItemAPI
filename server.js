628647da01a092a4c267b210805fde037baaa28b
Luay Sakr <LuaySakr@gmail.com>
2019-02-21 20:46:16 +0200

    update for test

--- server.js
+++ server.js
@@ -15,20 +15,16 @@ const appzip = require('appmetrics-zipkin')({
 });
 
 var opbeat = require('opbeat').start(
-// 	{
-//   appId: '873897962c',
-//   organizationId: 'c1db293267274544b523bc8e0182443c',
-//   secretToken: '3f4c16ad6b3f2453e9f5a04d94f58ce2fc081319'
-// }
+
 )
 
-app.use(morgan('dev'));                                         // log every request to the console
+app.use(morgan('dev'));                                         
 app.use(cors());
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({'extended':'true'}));           
+app.use(bodyParser.json());                                   
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 app.use(methodOverride());
-// app.use(opbeat.middleware.express())
+
 
 require('./app/router/router')(app);
 

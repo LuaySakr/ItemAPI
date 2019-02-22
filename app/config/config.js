5144ccf358142bd6928a4c5045fc6b41337a9f78
Luay Sakr <LuaySakr@gmail.com>
2019-02-22 04:33:54 +0200

    update configjs

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 var config = {
 	port: process.env.PORT || 2000,
-	db: process.env.MONGOLAB_URI || "mongodb://mongodb/smalitem",
+	db: process.env.MONGOLAB_URI || "mongodb://mongodb/smallitem",
 	test_port: 2001,
 	test_db: "mongodb://mongodb/smallitemapi_test"
 }

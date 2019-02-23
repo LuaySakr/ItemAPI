23ca0ae935e45ea601948d923126cc2ae32b0182
Luay Sakr <LuaySakr@gmail.com>
2019-02-24 01:23:14 +0200

    working

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 var config = {
-	port: process.env.PORT || 2000,
-	db: process.env.MONGOLAB_URI || "mongodb://mongodb/smallitem",
+	port: process.env.PORT || 7005,
+	db: process.env.MONGOLAB_URI || "mongodb://localhost/smallitem",
 	test_port: 2001,
 	test_db: "mongodb://mongodb/smallitemapi_test"
 }
--- app/controllers/smallitem.controller.js
+++ app/controllers/smallitem.controller.js
@@ -15,7 +15,94 @@ var BigItemCtrl = function(BigItem){
 		});
 	}
 
-	BigItemObj.GetBigItem = function(req, res, next){
+	 BigItemObj.GetBigItem = function(req, res, next){
+
+
+		const vehicleownerHostPort="http://localhost:7003/api/VehicleOwners"
+		const request = require("request");
+		const request2 = require("request");
+		var sleep = require('sleep');
+var options = {
+
+    uri: vehicleownerHostPort,
+
+    method: 'GET'
+	};
+
+	request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+     
+	//   console.log(JSON.parse(body).vehicleowner);
+	  
+	  JSON.parse(body).vehicleowner.forEach(element => {
+
+
+		  // console.log("old  "+element.status)
+		//   console.log("sucess")
+		  // console.log(element)
+		  const delay = require('delay');
+		   delay(1000);
+		 var newstatus= Math.random() >= 0.5
+		//  console.log("new  "+newstatus)
+		// sleep.sleep(1);
+
+  var options2 = {
+
+    uri: vehicleownerHostPort+"/"+element.number,
+
+    method: 'PUT',
+
+    json: {
+
+		_id:element._id,
+      number: element.number,
+
+      vehicleId: element.vehicleId,
+
+	  customerNum: element.customerNum,
+	  status:newstatus,
+	  completed:element.completed
+
+    }
+
+  };
+
+
+
+  console.log(options2.uri)
+  request(options2, function (error, response2, body) {
+
+    if (!error && response2.statusCode == 200) {
+console.log("sucess")
+
+
+//console.log(element)
+
+	}
+	else
+	{
+		console.log(response2.statusCode)
+	}
+
+  });
+
+
+			  
+
+
+	  });
+    }
+    else {
+      console.log(error);
+    }
+  })
+
+
+
+
+
+
+
 		BigItem.find(function(err, smallitem){
 			if(err) {
 				res.json({status: false, error: "Something went wrong"});
